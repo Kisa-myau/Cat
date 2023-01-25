0eac9075b08a60df52305fa27bfda34792cc035e
Anna Perechneva <perechneva_anna@mail.ru>
2023-01-25 14:57:20 +0400

    First task

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal{
+    public static String planet = "Луна";
+
+    public static void testStaticMethod(){
+        System.out.println("Это статическое животное");
+    }
+
+}
+
---
+++ src/Main3.java
@@ -0,0 +1,12 @@
+public class Main3 {
+    public static void main(String[] args) {
+// 3 task
+        Animal.testStaticMethod();
+        Cat.testStaticMethod();
+
+
+// 4 task
+        Animal murka = new Animal();
+        System.out.println(murka.planet);
+    }
+}
